9d2b7fb0aad90dcede26c290f9658bbda9c702e4
Alejandro Munoz McDonald <amunoz797@gmail.com>
2016-04-17 02:22:37 -0400

    Added skip functionality and changed youtube authentication
    OAuth 2.0 login removed, api key added.
    Users can now press skip button to skip currently playing media. Skip
    amount requirement hardcoded. TODO: Make skip amount based on percentage
    of connected users.

--- host.php
+++ host.php
@@ -1,5 +1,6 @@
 <!doctype html>
 <?php
+file_put_contents("skips.txt", "");
 if(file("songs-queued.txt")!=NULL){
 $songInfo = explode("~",file("songs-queued.txt")[0],3);
 }?>
@@ -26,10 +27,9 @@ $songInfo = explode("~",file("songs-queued.txt")[0],3);
 }</style>
   <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
   <script src="https://connect.soundcloud.com/sdk/sdk-3.0.0.js"></script>
-  <script src="js/auth.js"></script>
   <script src="js/search.js"></script>
   <script src="https://w.soundcloud.com/player/api.js"></script>
-    <script src="http://www.youtube.com/player_api"></script>
+    <script src="https://www.youtube.com/player_api"></script>
   <script>
   var url;
   var site = "<?php if(file("songs-queued.txt")!=NULL){echo trim(preg_replace('/\s+/', ' ', $songInfo[0]));}?>";
@@ -44,6 +44,17 @@ $songInfo = explode("~",file("songs-queued.txt")[0],3);
               id: place // various ways to store the ID, you can choose
             }
           });
+  }
+  var player;
+  function onPlayerReady(event) {
+    event.target.setVolume(100);
+    event.target.playVideo();
+    event.target.setPlaybackQuality('hd720');
+  }
+  function onPlayerStateChange(event){
+    if (event.data === 0) {
+      window.location.replace('next.php');
+    }
   }
   function play(){
     switch(site){
@@ -63,8 +74,19 @@ $songInfo = explode("~",file("songs-queued.txt")[0],3);
         break;
       case "YouTube":
         console.log("yOUtube");
+        player = new YT.Player('player', {
+        width: 854,
+        height: 480,
+        style:"display: block;margin-left: auto;margin-right: auto;",
+        videoId: "<?php if(file("songs-queued.txt")!=NULL){echo trim(preg_replace('/\s+/', ' ', $songInfo[1]));}?>",
+        events: {
+          'onReady': onPlayerReady,
+          'onStateChange': onPlayerStateChange,
+        }
+        });
+        $('#player').append('style="display: block;margin-left: auto;margin-right: auto;"');
         // document.getElementById('player').setAttribute("class","video-container");
-        $('#player').append('<iframe id="player" style="display: block;margin-left: auto;margin-right: auto;" type="text/html" width="854" height="480" src="http://www.youtube.com/embed/<?php if(file("songs-queued.txt")!=NULL){echo trim(preg_replace('/\s+/', ' ', $songInfo[1]));}?>?enablejsapi=1" frameborder="0"></iframe>');
+        // $('#player').append('<iframe id="player" style="display: block;margin-left: auto;margin-right: auto;" type="text/html" width="854" height="480" src="http://www.youtube.com/embed/<?php if(file("songs-queued.txt")!=NULL){echo trim(preg_replace('/\s+/', ' ', $songInfo[1]));}?>?enablejsapi=1" frameborder="0"></iframe>');
         break;
     }
     getNextSongs();
@@ -72,25 +94,47 @@ $songInfo = explode("~",file("songs-queued.txt")[0],3);
     }
 
     function getMedia() {
-    setTimeout(getMedia, 500);
     $.ajax({
-        url: 'songs-queued.txt',
+        url: 'songs-queued.txt?random='+Math.random(),
         dataType: 'text',
         success: function(text) {
+          setTimeout(getMedia, 2000);
           console.log(text);
-            if(text!=""){
-              location.reload();
-            }
+          if(text!=""){
+            location.reload();
+          }
         }
     })
     }
     var previousText="";
-    function getNextSongs() {
+    var lastText="";
     $.ajax({
         url: 'songs-queued.txt',
         dataType: 'text',
         success: function(text) {
-          setTimeout(getNextSongs, 500);
+          lastText=text;
+        }
+      });
+    function getNextSongs() {
+
+      $.ajax({
+          url: 'skips.txt?random='+Math.random(),
+          dataType: 'text',
+          success: function(text) {
+            document.getElementById('skipText').innerHTML = "Skips: "+text.length+'/4';
+            if(text.length>=4){
+              window.location.replace('next.php');
+            }
+          }
+        });
+    $.ajax({
+        url: 'songs-queued.txt?random='+Math.random(),
+        dataType: 'text',
+        success: function(text) {
+          setTimeout(getNextSongs, 2000);
+          if(text.split('\n')[0]!=lastText.split('\n')[0]){
+            location.reload();
+          }
           if(text!=previousText){
               $('#nextSongs').empty();
               $('#nextSongs').append("<thead><tr><th>Up Next</th></tr></thead><tbody>");
@@ -117,7 +161,6 @@ $songInfo = explode("~",file("songs-queued.txt")[0],3);
   <meta charset="utf-8" />
     <title>Play</title>
   </head>
-
   <?php
   if(file("songs-queued.txt")!=NULL){
   ?>
@@ -125,8 +168,13 @@ $songInfo = explode("~",file("songs-queued.txt")[0],3);
     <div class="container">
       <div id="player">
       </div><br>
-      <button id="nextButton" class="button-primary" style="float:right;" onclick="window.location.replace('next.php');">Next</button>
-      <br>
+      <div class="row">
+        <div id="skips" class="four columns">
+          <h2 id="skipText">Skips:</h2>
+        </div>
+        <button id="nextButton" class="button-primary" style="float:right;" onclick="window.location.replace('next.php');">Next</button>
+        <br>
+      </div>
       <table class='u-full-width' id='nextSongs'></table>
     </div>
   </body>
@@ -134,7 +182,7 @@ $songInfo = explode("~",file("songs-queued.txt")[0],3);
   <body onload="getMedia();">
     <div class="container">
       <div class="row">
-        <div class="twelve columns"><h1 style='text-align:center;'>Queue Song</h1>
+        <div class="twelve columns"><h1 style='text-align:center;'>Queue Song</h1><br><h1 style='text-align:center;'>connect to MessWithTheBest</h1><br><h1 style='text-align:center;'>go to 192.168.2.144/DJ</h1>
       </div>
     </div>
   </body>
---
+++ skip.php
@@ -0,0 +1,2 @@
+<?php
+  file_put_contents("skips.txt","1",FILE_APPEND);?>
